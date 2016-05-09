6a2426ddc0e0ecca09c5eb8b05c5107f4c5de55e
Your Name <blok.artwork@googlemail.com>
2016-05-09 14:22:41 +0200

    create shape
    You can now create whatever shape you like in the grid.

--- build/build-tmp/source/build.java
+++ build/build-tmp/source/build.java
@@ -25,20 +25,23 @@ public void setup() {
 public void draw() {
 
 	grid1.createGrid();
-	
+
 }
 
 class Grid {
 
 	int gridSize = 20;
 
-
 	public void createGrid() {
 		for (int i = 0; i < width; i += gridSize) {
 			for (int j = 0; j < height; j += gridSize) {
-				rect(i, j, gridSize, gridSize);
+				drawShape(i, j, gridSize, gridSize);
 			}
 		}
+	}
+
+	public void drawShape(int posX, int posY, int shapeWidth, int shapeHeight) {
+		rect(posX, posY, shapeWidth, shapeHeight);
 	}
 }
   public void settings() { 	size(1920, 1080); }
--- build/build.pde
+++ build/build.pde
@@ -19,8 +19,12 @@ class Grid {
 	void createGrid() {
 		for (int i = 0; i < width; i += gridSize) {
 			for (int j = 0; j < height; j += gridSize) {
-				rect(i, j, gridSize, gridSize);
+				drawShape(i, j, gridSize, gridSize);
 			}
 		}
+	}
+
+	void drawShape(int posX, int posY, int shapeWidth, int shapeHeight) {
+		rect(posX, posY, shapeWidth, shapeHeight);
 	}
 }
