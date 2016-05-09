ba50a78f6702a3c369f7bf7c2d7cb5a32a0385e4
Your Name <blok.artwork@googlemail.com>
2016-05-09 14:43:52 +0200

    set color and grid
    You can now set the color of the shape and change the shape size
    independently from the grid size.

--- build/build-tmp/source/build.java
+++ build/build-tmp/source/build.java
@@ -19,29 +19,42 @@ Grid grid1;
 public void setup() {
 	
 
+	colorMode(HSB, 360, 100, 100);
+
 	grid1 = new Grid();
+
+	grid1.createGrid();
 }
 
 public void draw() {
-
-	grid1.createGrid();
 
 }
 
 class Grid {
 
 	int gridSize = 20;
+	int shapeSize = 10;
 
 	public void createGrid() {
 		for (int i = 0; i < width; i += gridSize) {
 			for (int j = 0; j < height; j += gridSize) {
-				drawShape(i, j, gridSize, gridSize);
+				setShape();
+				drawShape(i, j, shapeSize, shapeSize);
 			}
 		}
 	}
 
+	// That defines what kind of shape you want to draw
+
 	public void drawShape(int posX, int posY, int shapeWidth, int shapeHeight) {
 		rect(posX, posY, shapeWidth, shapeHeight);
+	}
+
+	// That takes all information about the shape
+
+	public void setShape() {
+		fill(random(360), 100, 80);
+		noStroke();
 	}
 }
   public void settings() { 	size(1920, 1080); }
--- build/build.pde
+++ build/build.pde
@@ -3,28 +3,37 @@ Grid grid1;
 void setup() {
 	size(1920, 1080);
 
+	colorMode(HSB, 360, 100, 100);
+
 	grid1 = new Grid();
+
+	grid1.createGrid();
 }
 
 void draw() {
-
-	grid1.createGrid();
 
 }
 
 class Grid {
 
 	int gridSize = 20;
+	int shapeSize = 10;
 
 	void createGrid() {
 		for (int i = 0; i < width; i += gridSize) {
 			for (int j = 0; j < height; j += gridSize) {
-				drawShape(i, j, gridSize, gridSize);
+				setShape();
+				drawShape(i, j, shapeSize, shapeSize);
 			}
 		}
 	}
 
 	void drawShape(int posX, int posY, int shapeWidth, int shapeHeight) {
 		rect(posX, posY, shapeWidth, shapeHeight);
+	}
+
+	void setShape() {
+		fill(random(360), 100, 80);
+		noStroke();
 	}
 }
